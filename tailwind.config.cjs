945a6e1bca605084d65e3f3e1ec60c1fd867decf
jordigd20 <jordigd8@gmail.com>
2023-05-19 19:26:44 +0200

    tailwind + nav component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--color-background)',
+        textColor: 'var(--color-text)',
+      }
+    },
+    fontFamily: {
+      sans: ['Rubik', ...defaultTheme.fontFamily.sans],
+    }
+  },
+  darkMode: 'class',
+  plugins: []
+};
