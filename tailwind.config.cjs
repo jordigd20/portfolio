a0c8f1b12ed475d872f67f5c5f933effc2069663
jordigd20 <jordigd8@gmail.com>
2023-05-20 19:03:49 +0200

    hero section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,32 @@ module.exports = {
       colors: {
         background: 'var(--color-background)',
         textColor: 'var(--color-text)',
+      },
+      animation: {
+        'fade-in-down': 'fade-in-down 350ms ease-out',
+        'fade-out-up': 'fade-out-up 350ms ease-out',
+      },
+      keyframes: {
+        'fade-in-down': {
+          from: {
+            opacity: '0',
+            transform: 'translateY(-100%)'
+          },
+          to: {
+            opacity: '1',
+            transform: 'translateY(0)'
+          }
+        },
+        'fade-out-up': {
+          from: {
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+          to: {
+            opacity: '0',
+            transform: 'translateY(-100%)'
+          }
+        },
       }
     },
     fontFamily: {
