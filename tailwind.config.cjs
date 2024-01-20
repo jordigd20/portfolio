a1d6fb3ec6212eca755d627485643e22fc614760
jordigd20 <jordigd8@gmail.com>
2024-01-20 14:01:53 +0100

    fix: ui primary button style for dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -33,15 +33,7 @@ module.exports = {
       animation: {
         'slide-in': 'slide-in 300ms cubic-bezier(0.6, 0.6, 0, 1)',
         'slide-out': 'slide-out 300ms cubic-bezier(0.6, 0.6, 0, 1)',
-        'fade-in-down': 'fade-in-down 250ms ease-out',
-        'fade-out-up': 'fade-out-up 250ms ease-out',
         'fade-in-up': 'fade-in-up 350ms ease-out',
-        'enter-from-right': 'enter-from-right 200ms ease-in-out',
-        'enter-from-left': 'enter-from-left 200ms ease-in-out',
-        'leave-to-left': 'leave-to-left 200ms ease-in-out',
-        'leave-to-right': 'leave-to-right 200ms ease-in-out',
-        'fade-in': 'fade-in 250ms ease-in-out',
-        'fade-out': 'fade-out 250ms ease-in-out'
       },
       keyframes: {
         'slide-in': {
@@ -60,26 +52,6 @@ module.exports = {
             transform: 'translateX(-100%)',
           }
         },
-        'fade-in-down': {
-          from: {
-            opacity: '0',
-            transform: 'translateY(-100%)'
-          },
-          to: {
-            opacity: '1',
-            transform: 'translateY(0)'
-          }
-        },
-        'fade-out-up': {
-          from: {
-            opacity: '1',
-            transform: 'translateY(0)'
-          },
-          to: {
-            opacity: '0',
-            transform: 'translateY(-100%)'
-          }
-        },
         'fade-in-up': {
           from: {
             opacity: '0',
@@ -90,62 +62,6 @@ module.exports = {
             transform: 'translateY(0)'
           }
         },
-        'enter-from-right': {
-          from: {
-            transform: 'translateX(3rem)',
-            opacity: '0'
-          },
-          to: {
-            transform: 'translateX(0)',
-            opacity: '1'
-          }
-        },
-        'enter-from-left': {
-          from: {
-            transform: 'translateX(-3rem)',
-            opacity: '0'
-          },
-          to: {
-            transform: 'translateX(0)',
-            opacity: '1'
-          }
-        },
-        'leave-to-left': {
-          from: {
-            transform: 'translateX(0)',
-            opacity: '1'
-          },
-          to: {
-            transform: 'translateX(-3rem)',
-            opacity: '0'
-          }
-        },
-        'leave-to-right': {
-          from: {
-            transform: 'translateX(0)',
-            opacity: '1'
-          },
-          to: {
-            transform: 'translateX(3rem)',
-            opacity: '0'
-          }
-        },
-        'fade-in': {
-          from: {
-            opacity: '0'
-          },
-          to: {
-            opacity: '1'
-          }
-        },
-        'fade-out': {
-          from: {
-            opacity: '1'
-          },
-          to: {
-            opacity: '0'
-          }
-        }
       },
       listStyleType: {
         revert: 'revert'
