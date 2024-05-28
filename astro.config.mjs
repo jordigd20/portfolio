3ecb1c95dbb18ba9340c471e65a3bd3279650d88
jordigd20 <jordigd8@gmail.com>
2024-05-28 13:23:19 +0200

    fix: remove vercel serverless functions due to reaching the max functions on the free plan. Reverting the site to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,13 @@
 import { defineConfig, squooshImageService } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'static',
   integrations: [tailwind()],
   image: {
     service: squooshImageService(),
   },
-  adapter: vercel({
-    analytics: true,
-  }),
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
